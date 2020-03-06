bde143334ab4b5e7d7dc24cbd0885df7b151a51d
Ulises <ulises.rogo@gmail.com>
2020-03-06 13:00:47 -0600

    Cambia contenido de página Error 404

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -2,7 +2,7 @@ const Error404 = () => {
   const view = `
     <div class="Error404">
       <h2>Error 404</h2>
-      <p>Parece que hubo un error :(</p>
+      <p>Parece que la parte a donde quieres acceder no existe :(</p>
     </div>
   `;
   return view;
