fdc52eb0767bfa9f42c448296766b1ced336a0c5
Ulises <ulises.rogo@gmail.com>
2020-03-06 20:40:53 -0600

    2a parte de refactorización de filtrado de perros

--- src/utils/filterDogs.js
+++ src/utils/filterDogs.js
@@ -37,24 +37,64 @@ const filterDogs = () => {
 
       const cssClassSize = ev.target.id.replace('btn-', '.')
       // console.log(cssClassSize)
-      if (activeCategories.indexOf(cssClassSize) === -1) {
-        activeCategories.push(cssClassSize);
-      } else {
-        activeCategories = activeCategories.filter(
-          cat => cat.indexOf(cssClassSize) === -1
-        );
-      }
+      
+      setActiveCategories(cssClassSize)
+     
 
       cssClassesSize = activeCategories.join(', ');
+
+
+
+
+      if (activeCategories.length === 0) {
+        showAllDogs();
+        // for (let i = 0; i < $allDogs.length; i++) {
+        //   $allDogs[i].classList.remove('hidden');
+        // }
+      } else {
+        const excludeClassQuery = ':not(' + activeCategories.join('):not(') + ')';
+        
+        const $elementsToHide = document.querySelectorAll(
+          '.dog' + excludeClassQuery
+        );
+        for (let i = 0; i < $elementsToHide.length; i++) {
+          $elementsToHide[i].classList.add('hidden');
+        }
+  
+        const $elementsToShow = document.querySelectorAll(cssClassesSize);
+        for (let i = 0; i < $elementsToShow.length; i++) {
+          $elementsToShow[i].classList.remove('hidden');
+        }
+      }
 
 
     })
   })
 
+  function setActiveCategories(cssClassSize) {
+    if (activeCategories.indexOf(cssClassSize) === -1) {
+      activeCategories.push(cssClassSize);
+    } else {
+      activeCategories = activeCategories.filter(
+        cat => cat.indexOf(cssClassSize) === -1
+      );
+    }
+  }
+
+  function showAllDogs() {
+    for (let i = 0; i < $allDogs.length; i++) {
+      $allDogs[i].classList.remove('hidden');
+    }
+  }
+  
+
+};
 
 
 
-  $buttonS.addEventListener('click', ev => {
+
+
+  // $buttonS.addEventListener('click', ev => {
     // ev.currentTarget.classList.toggle('on');
 
     // if (activeCategories.indexOf('.small') === -1) {
@@ -67,28 +107,28 @@ const filterDogs = () => {
 
     // cssClasses = activeCategories.join(', ');
 
-    if (activeCategories.length === 0) {
-      for (let i = 0; i < $allDogs.length; i++) {
-        $allDogs[i].classList.remove('hidden');
-      }
-    } else {
-      let excludeClassQuery = ':not(' + activeCategories.join('):not(') + ')';
+    // if (activeCategories.length === 0) {
+    //   for (let i = 0; i < $allDogs.length; i++) {
+    //     $allDogs[i].classList.remove('hidden');
+    //   }
+    // } else {
+    //   let excludeClassQuery = ':not(' + activeCategories.join('):not(') + ')';
 
-      const $elementsToHide = document.querySelectorAll(
-        '.dog' + excludeClassQuery
-      );
-      for (let i = 0; i < $elementsToHide.length; i++) {
-        $elementsToHide[i].classList.add('hidden');
-      }
+    //   const $elementsToHide = document.querySelectorAll(
+    //     '.dog' + excludeClassQuery
+    //   );
+    //   for (let i = 0; i < $elementsToHide.length; i++) {
+    //     $elementsToHide[i].classList.add('hidden');
+    //   }
 
-      const $elementsToShow = document.querySelectorAll(cssClassesSize);
-      for (let i = 0; i < $elementsToShow.length; i++) {
-        $elementsToShow[i].classList.remove('hidden');
-      }
-    }
-  });
+    //   const $elementsToShow = document.querySelectorAll(cssClassesSize);
+    //   for (let i = 0; i < $elementsToShow.length; i++) {
+    //     $elementsToShow[i].classList.remove('hidden');
+    //   }
+    // }
+  // });
 
-  $buttonM.addEventListener('click', ev => {
+  // $buttonM.addEventListener('click', ev => {
     // ev.currentTarget.classList.toggle('on');
 
     // if (activeCategories.indexOf('.medium') === -1) {
@@ -101,29 +141,29 @@ const filterDogs = () => {
 
     // cssClasses = activeCategories.join(', ');
 
-    if (activeCategories.length === 0) {
-      for (let i = 0; i < $allDogs.length; i++) {
-        $allDogs[i].classList.remove('hidden');
-      }
-    } else {
-      let excludeClassQuery = ':not(' + activeCategories.join('):not(') + ')';
+    // if (activeCategories.length === 0) {
+    //   for (let i = 0; i < $allDogs.length; i++) {
+    //     $allDogs[i].classList.remove('hidden');
+    //   }
+    // } else {
+    //   let excludeClassQuery = ':not(' + activeCategories.join('):not(') + ')';
 
-      const $elementsToHide = document.querySelectorAll(
-        '.dog' + excludeClassQuery
-      );
-      // console.log($elementsToHide);
-      for (let i = 0; i < $elementsToHide.length; i++) {
-        $elementsToHide[i].classList.add('hidden');
-      }
+    //   const $elementsToHide = document.querySelectorAll(
+    //     '.dog' + excludeClassQuery
+    //   );
+    //   // console.log($elementsToHide);
+    //   for (let i = 0; i < $elementsToHide.length; i++) {
+    //     $elementsToHide[i].classList.add('hidden');
+    //   }
 
-      const $elementsToShow = document.querySelectorAll(cssClassesSize);
-      for (let i = 0; i < $elementsToShow.length; i++) {
-        $elementsToShow[i].classList.remove('hidden');
-      }
-    }
-  });
+    //   const $elementsToShow = document.querySelectorAll(cssClassesSize);
+    //   for (let i = 0; i < $elementsToShow.length; i++) {
+    //     $elementsToShow[i].classList.remove('hidden');
+    //   }
+    // }
+  // });
 
-  $buttonB.addEventListener('click', ev => {
+  // $buttonB.addEventListener('click', ev => {
     // ev.currentTarget.classList.toggle('on');
 
     // if (activeCategories.indexOf('.big') === -1) {
@@ -136,26 +176,26 @@ const filterDogs = () => {
 
     // cssClasses = activeCategories.join(', ');
 
-    if (activeCategories.length === 0) {
-      for (let i = 0; i < $allDogs.length; i++) {
-        $allDogs[i].classList.remove('hidden');
-      }
-    } else {
-      let excludeClassQuery = ':not(' + activeCategories.join('):not(') + ')';
+    // if (activeCategories.length === 0) {
+    //   for (let i = 0; i < $allDogs.length; i++) {
+    //     $allDogs[i].classList.remove('hidden');
+    //   }
+    // } else {
+    //   let excludeClassQuery = ':not(' + activeCategories.join('):not(') + ')';
 
-      const $elementsToHide = document.querySelectorAll(
-        '.dog' + excludeClassQuery
-      );
-      for (let i = 0; i < $elementsToHide.length; i++) {
-        $elementsToHide[i].classList.add('hidden');
-      }
+    //   const $elementsToHide = document.querySelectorAll(
+    //     '.dog' + excludeClassQuery
+    //   );
+    //   for (let i = 0; i < $elementsToHide.length; i++) {
+    //     $elementsToHide[i].classList.add('hidden');
+    //   }
 
-      const $elementsToShow = document.querySelectorAll(cssClassesSize);
-      for (let i = 0; i < $elementsToShow.length; i++) {
-        $elementsToShow[i].classList.remove('hidden');
-      }
-    }
-  });
-};
+    //   const $elementsToShow = document.querySelectorAll(cssClassesSize);
+    //   for (let i = 0; i < $elementsToShow.length; i++) {
+    //     $elementsToShow[i].classList.remove('hidden');
+    //   }
+    // }
+  // });
+
 
 export default filterDogs;
